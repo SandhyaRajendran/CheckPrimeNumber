9279fbc3de52848a8fbe9aa22bbcdf93530555b9
SandhyaRajendran <sandhyadckap@gmail.com>
2023-05-25 14:53:05 +0530

    alindrome other way

--- palindrome.php
+++ palindrome.php
@@ -17,3 +17,56 @@ pallindrome("rubber");
 pallindrome("amma");
 pallindrome("radar");
 pallindrome("malayalam");
+
+
+//other way 
+
+// The program to find a string is palindrome or not is :
+
+
+
+function Palindrome($number){ 
+
+    $temp = $number; 
+
+    $new = 0; 
+
+    while (floor($temp)) { 
+
+        $d = $temp % 10; 
+
+        $new = $new * 10 + $d; 
+
+        $temp = $temp/10; 
+
+    } 
+
+    if ($new == $number){ 
+
+        return 1; 
+
+    }
+
+    else{
+
+        return 0;
+
+    }
+
+} 
+
+$original = 257752;
+
+if (Palindrome($original)){ 
+
+    echo "Palindrome"; 
+
+}
+
+else { 
+
+echo "Not a Palindrome"; 
+
+}
+
+?> 
