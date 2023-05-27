5eebf5d0c8de2e4fc5429f645030d0f222c2703b
SandhyaRajendran <sandhyadckap@gmail.com>
2023-05-27 10:47:54 +0530

    fibonacci completed

--- fibonnacii.php
+++ fibonnacii.php
@@ -3,10 +3,12 @@
 //Write a PHP Program to generate Fibonacci series?
 //first method
 $userValue = (int)readline("Enter a number : ");
-echo "userValue";
+// echo "userValue";
 echo $userValue."\n";
 $startingValue  = 0;
 $secondValue = 1;
+
+
 echo $startingValue."\n";
 echo $secondValue."\n";
 
@@ -39,3 +41,25 @@ while ($nextTerm <= $fromUser) {
     $n2 = $nextTerm;;
     $nextTerm = $n1+$n2;
 }
+
+//it shows the till the count of userInput ex: 5 its output is 0,1,2,3,5
+//it shows the till the count of userInput ex: 8 its output is 0,1,2,3,5,8,13,21
+
+// $number = (int)readline('Enter the number of terms: ');
+// $n1 = 0;
+// $n2 = 1;
+// $nextTerm;
+
+// echo('Fibonacci Series:');
+
+// for ($i = 1; $i < $number; $i++) {
+//     // echo($n1."\n");
+//     // console.log(i+" "+number)
+//     // console.log(i+" "+number)
+//     echo $nextTerm."\n";
+
+//     $n1 = $n2;
+//     $n2 = $nextTerm;
+//     $nextTerm = $n1 + $n2;
+
+// }
