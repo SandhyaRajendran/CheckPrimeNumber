3af2de1d411aefca65da7a34ceee548ccd2a166e
SandhyaRajendran <sandhyadckap@gmail.com>
2023-05-31 20:00:29 +0530

    find leap year complleted

---
+++ checkLeapYear.php
@@ -0,0 +1,28 @@
+<?php
+    $my_year = readline("Enter a year : ");
+   function year_check($my_year){
+      if ($my_year % 400 == 0)
+         print("It is a leap year");
+      else if ($my_year % 100 == 0)
+         print("It is not a leap year");
+      else if ($my_year % 4 == 0)
+         print("It is a leap year");
+      else
+         print("It is not a leap year");
+   }
+   
+   year_check($my_year);
+
+   //other way
+
+$year = 2032;  
+ 
+if((0 == $year % 4) & (0 != $year % 100) | (0 == $year % 400))
+{
+echo "$year is a Leap Year.";    
+}
+ 
+else  
+{  
+echo "$year is not a Leap Year.";    
+}
